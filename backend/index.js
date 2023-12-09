6ea12d799b6a7c1eeec18f9892fbf913e65aadb4
udhaykumarbala <udhayk97@gmail.com>
2023-12-09 10:19:28 +0530

    routes added

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,40 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+// Route to get address
+app.get('/getAddress', (req, res) => {
+  const id = req.query.id;
+  getAddress(id, (err, address) => {
+      if (err) {
+          res.status(500).send('Error retrieving address');
+      } else {
+          const response = {
+              address: address,
+          };
+          res.send(response);
+      }
+  });
+});
+
+// Route to add address
+app.post('/addAddress', (req, res) => {
+  const signature = req.body.signature;
+
+  const message = req.body.message;
+  const address = verifySignedMessage(message, signature);
+  addAddress(address, (err, id) => {
+      if (err) {
+          res.status(500).send('Error adding address');
+      } else {
+          const response = {
+              id: id,
+              address: address,
+          };
+          res.send(response);
+      }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
