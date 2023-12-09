7c514603b92bbc73991cc65979f460eae1b1683c
Gathin <gathint23@gmail.com>
2023-12-09 10:28:17 +0530

    Added Components

---
+++ frontend/src/component/ConnectButton.js
@@ -0,0 +1,7 @@
+export default function ConnectButton() {
+  return (
+    <div className="mt-5 flex justify-end mr-10">
+      <w3m-button />
+    </div>
+  );
+}
---
+++ frontend/src/component/InfoCollections.js
@@ -0,0 +1,11 @@
+import InfoDisplay from "./InfoDisplay";
+
+export default function InfoCollections() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <InfoDisplay name="Lens Protocol" />
+      <InfoDisplay name="Farcaster" />
+      <InfoDisplay name=""/>
+    </div>
+  );
+}
