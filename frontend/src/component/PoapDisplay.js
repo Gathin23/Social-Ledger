df02bab37747470d0ac2fdb17bb8077b0d18c746
Gathin <gathint23@gmail.com>
2023-12-10 01:58:16 +0530

    background colour and poap orientation

--- frontend/src/component/PoapDisplay.js
+++ frontend/src/component/PoapDisplay.js
@@ -3,7 +3,7 @@ export default function PoapDisplay({items}) {
         <div className="flex flex-wrap">
             {items.map((item) => (
                 <div className="w-full sm:w-1/2 p-4">
-                    <div className="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm">
+                    <div className="max-w-md mx-auto mt-4 shadow-lg border rounded-md hover:bg-gray-200 duration-300">
                         <a href={item.href}>
                             <div className="pt-3 ml-4 mr-2 mb-3">
                                 <h3 className="text-xl text-gray-900">
