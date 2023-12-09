c689811a5bb70137b5c2228f62040f4b227ae69e
Gathin <gathint23@gmail.com>
2023-12-09 10:21:15 +0530

    Added WalletConnect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createWeb3Modal, defaultConfig } from "@web3modal/ethers5/react";
+import ConnectButton from "./component/ConnectButton";
+import InfoCollections from "./component/InfoCollections";
+
+const projectId = "3c73a77b6b4f6e3b4550743cc4a863ae";
+
+// 2. Set chains
+const mumbai = {
+  chainId: 80001,
+  name: "Mumbai",
+  currency: "MATIC",
+  explorerUrl: "https://mumbai.polygonscan.com",
+  rpcUrl: "https://rpc-mumbai.maticvigil.com",
+};
+
+// 3. Create modal
+const metadata = {
+  name: "Link In Bio",
+  description:
+    " Building a link-in-bio solution that cuts across Lens, Farcaster, XMTP and other onchain and offchain activities and accounts of the user.",
+  url: "",
+  icons: [""],
+};
+
+createWeb3Modal({
+  ethersConfig: defaultConfig({ metadata }),
+  chains: [mumbai],
+  projectId,
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <ConnectButton />
+      <InfoCollections />
     </div>
   );
 }
